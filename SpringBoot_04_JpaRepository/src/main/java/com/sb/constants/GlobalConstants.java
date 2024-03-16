38af9ff1840093ed5545a565d772816ffd936552
Swapnil-197 <sspatil1907@gmail.com>
2024-03-16 15:18:11 +0530

    added company standards declaring constants in GlobConst class

--- SpringBoot_04_JpaRepository/src/main/java/com/sb/constants/GlobalConstants.java
+++ SpringBoot_04_JpaRepository/src/main/java/com/sb/constants/GlobalConstants.java
@@ -6,6 +6,13 @@ public class GlobalConstants {
 		
 	}
 	
+	public static final String WELCOME_MESSAGE="Welcome to Spring Boot Application..!!";
+	public static final String ROOT_MAPPING="/";
 	public static final String STUDENT_CONTROLLER_MAPPING = "/student";
+	public static final String STUDENT_GET_ALL="/getAllStudents";
+	public static final String STUDENT_ADD_STUDENT="/addStudent";
+	public static final String STUDENT_UPDATE_STUDENT="/updateStudent";
+	public static final String STUDENT_GET_BY_ID="/getStudentById/{studentId}";
+	public static final String STUDENT_DELETE_BY_ID="/deleteStudentById/{studentId}";
 
 }
