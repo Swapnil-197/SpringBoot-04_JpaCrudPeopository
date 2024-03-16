432d135e627b7857a18812d40215914fe30d229c
Swapnil-197 <sspatil1907@gmail.com>
2024-03-16 15:20:43 +0530

    GlobalConst class variable related changes in Controller

--- SpringBoot_04_JpaRepository/src/main/java/com/sb/controller/StudentController.java
+++ SpringBoot_04_JpaRepository/src/main/java/com/sb/controller/StudentController.java
@@ -23,36 +23,37 @@ public class StudentController {
 	@Autowired
 	StudentService studentService;
 	
-	@GetMapping("/getAllStudents")
+	@GetMapping(GlobalConstants.STUDENT_GET_ALL)
 	public List<Student> getAllStudents(){
 		return studentService.getAllStudents();
 	}
 	
 	//In this method we are passing input from postman to see on console with the help of
 	// PostMappnig and RequestBody annotation
-	@PostMapping(path = "/addStudent")
+	@PostMapping(GlobalConstants.STUDENT_ADD_STUDENT)
 	public Student addStudent(@RequestBody Student student) {
 		System.out.println("Data added in Database");
 		return studentService.addStudent(student);
 	}
 	
-	@PutMapping(path = "/updateStudent")
+	@PutMapping(GlobalConstants.STUDENT_UPDATE_STUDENT)
 	public Student updateStudent(@RequestBody Student student) {
 		System.out.println(student);
 		return studentService.updateStudent(student);
 	}
 	
 	//from UI(from POSTMAN we can send data through URL with the help of @PathVariable annotation)
-	@GetMapping(path = "/getStudentById/{studentId}")
+	@GetMapping(GlobalConstants.STUDENT_GET_BY_ID)
 	public Optional<Student> getStudentById(@PathVariable Integer studentId) {
 		System.out.println(studentId);
 	    return studentService.getStudentById(studentId);
 	}
 	
-	@DeleteMapping(path = "/deleteStudentById/{studentId}")
+	@DeleteMapping(GlobalConstants.STUDENT_DELETE_BY_ID)
 	public void deleteStudentById(@PathVariable Integer studentId) {
 		System.out.println(studentId);
 		studentService.deleteStudentById(studentId);
+		
 	}
 	
 }
